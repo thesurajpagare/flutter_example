9448d361a86a12324d685746a0e4613974e6f4c1
thesurajpagare <surajpagare75@gmail.com>
2021-09-07 15:51:26 +0530

    Loginchceck added

--- lib/Login_Screen.dart
+++ lib/Login_Screen.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
-//import 'package:samplenew_project/SignUpScreen.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'DashBoardScreen.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -14,10 +16,31 @@ class _State extends State<LoginPage> {
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   late Future<String> futureAlbum;
+  late Future<String> futureLoginCheck;
+  var GUID=null;
+  late SharedPreferences prefs;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    asyncMethod();
+  }
+
+  //get dat from shared preferences
+  void asyncMethod() async {
+    prefs =  await SharedPreferences.getInstance();
+    GUID= prefs.getString('GUID').toString();
+    print("guid from pref"+GUID);
+    
+    // ....
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return
+
+      Scaffold(
         appBar: AppBar(
           title: Text('Login Screen'),
         ),
@@ -75,7 +98,7 @@ class _State extends State<LoginPage> {
                       color: Colors.blue,
                       child: Text('Login'),
                       onPressed: () {
-                        futureAlbum = fetchAlbum();
+                        futureAlbum = getLogin(context,nameController.text,passwordController.text,GUID);
 
                         print(nameController.text);
                         print(passwordController.text);
@@ -92,8 +115,8 @@ class _State extends State<LoginPage> {
                             style: TextStyle(fontSize: 20),
                           ),
                           onPressed: () {
-
-                            Navigator.push(
+                           // Navigator.of(context,rootNavigator: true).pop();
+                            Navigator.pushReplacement(
                               context,
                               MaterialPageRoute(
                                   builder: (context) => LoginPage()),
@@ -106,59 +129,204 @@ class _State extends State<LoginPage> {
                     ))
               ],
             )));
+    
   }
 
 }
 
 
-Future<String> fetchAlbum() async {
-  print("in future class");
-  /*final response = await http
-      .post(Uri.parse('https://acmeitsolutions.net/projects/freightseek/api/registerDevice'));
-*/
-  var json_body = { 'GUID': 'NEW'};
+
+//API Call Login Post method
+Future<String> getLogin(BuildContext ctx,String userName,String password,String GUID) async {
+  var jsonBody = { 'username': userName,'password':password,'GUID':GUID};
   Map<String, String> headers = {"Content-type": "application/x-www-form-urlencoded"};
-  var baseUrl = 'http://acmeitservices.com/projects/freightseek/api/';
-  final uri = Uri.parse("https://acmeitsolutions.net/projects/freightseek/api/registerDevice");
-  final response = await http.post(uri, headers: headers, body: json_body);
+  final uri = Uri.parse("https://acmeitsolutions.net/projects/freightseek/api/login");
+  final response = await http.post(uri, headers: headers, body: jsonBody);
   print("in future class after api"+response.body);
 
   if (response.statusCode == 200) {
     var data = json.decode(response.body);
-    var userID = data["data"];
-    print("in future class 200 "+userID["GUID"]);
+    print(data["ResponseCode"]);
+    if(data["ResponseCode"]=="200" && data["Success"]=="true") {
+      var message=data["Message"];
+      var dataUser = data["data"];
+      var Userid=dataUser["Userid"];
+      var Mobile=dataUser["Mobile"];
+      var Name=dataUser["Name"];
+      var user_type=dataUser["user_type"];
+      var Email=dataUser["Email"];
+      var AccessToken=dataUser["AccessToken"];
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('Userid',Userid);
+      prefs.setString('Mobile',Mobile);
+      prefs.setString('Name',Name);
+      prefs.setString('user_type',user_type);
+      prefs.setString('Email',Email);
+      prefs.setString('AccessToken',AccessToken);
+      Fluttertoast.showToast(
+          msg:message,
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIos: 1
+      );
+
+  //    _showMyDialog(ctx,message);
+     /* print("in future class 200 " + dataGUID["GUID"]);
+      String Userid = jsonObjectUser.getString("Userid");
+      String Mobile = jsonObjectUser.getString("Mobile");
+      String Name = jsonObjectUser.getString("Name");
+      String user_type = jsonObjectUser.getString("user_type");
 
 
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
+      String Email = jsonObjectUser.getString("Email");
+
+      String AccesToken = jsonObjectUser.getString("AccessToken");
+      if (jsonObjectUser.has("member_status")) {
+        String member_status = jsonObjectUser.getString("member_status");
+        if (user_type.trim().equalsIgnoreCase("carrier")) {
+          System.out.println("car===" + member_status);
+          sessionManager.setTransporterStatus(member_status);
+        }
+      }*/
+
+
+
+    }else if(data["ResponseCode"]==400)
+      {
+        //print("code==="+data["ResponseCode"]);
+        var datauserID=data["data"];
+        String userid=datauserID["Userid"].toString();
+        _showMyDialog(ctx,data["Message"],userid,GUID);
+      }
+
+
+
+     // "ResponseCode":400,
+
     return "";
+    //return Album.fromJson(jsonDecode(response.body));
   } else {
     print("in future class excption ");
-
     // If the server did not return a 200 OK response,
     // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
 
-class Album {
-  final int userId;
-  final int id;
-  final String title;
 
-  Album({
-    required this.userId,
-    required this.id,
-    required this.title,
-  });
 
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-    );
+
+//API Call Logincheck Post method
+Future<String> getLoginCheck(BuildContext ctx,String userID,String GUID) async {
+  var jsonBody = { 'userId': userID,'GUID':GUID};
+  Map<String, String> headers = {"Content-type": "application/x-www-form-urlencoded"};
+  final uri = Uri.parse("https://acmeitsolutions.net/projects/freightseek/api/loginCheck");
+  final response = await http.post(uri, headers: headers, body: jsonBody);
+ // print("in Logincheck after api"+response.body);
+
+  if (response.statusCode == 200) {
+    var data = json.decode(response.body);
+    if(data["ResponseCode"]==200 && data["Success"]=="true") {
+      print("in Logincheck after api"+response.body);
+      var message=data["Message"];
+      var dataUser = data["data"];
+      var Userid=dataUser["Userid"];
+      var Mobile=dataUser["Mobile"];
+      var Name=dataUser["Name"];
+      var user_type=dataUser["user_type"];
+      var Email=dataUser["Email"];
+      var AccessToken=dataUser["AccessToken"];
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('Userid',Userid.toString());
+      prefs.setString('Mobile',Mobile);
+      prefs.setString('Name',Name);
+      prefs.setString('user_type',user_type);
+      prefs.setString('Email',Email);
+      prefs.setString('AccessToken',AccessToken);
+      Fluttertoast.showToast(
+          msg:message,
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIos: 1
+      );
+    // Navigator.of(ctx,rootNavigator: true).pop();
+      Navigator.pushReplacement(
+        ctx,
+        MaterialPageRoute(
+            builder: (context) => DashBoardScreen()),
+      );
+
+      /* print("in future class 200 " + dataGUID["GUID"]);
+      String Userid = jsonObjectUser.getString("Userid");
+      String Mobile = jsonObjectUser.getString("Mobile");
+      String Name = jsonObjectUser.getString("Name");
+      String user_type = jsonObjectUser.getString("user_type");
+
+
+      String Email = jsonObjectUser.getString("Email");
+
+      String AccesToken = jsonObjectUser.getString("AccessToken");
+      if (jsonObjectUser.has("member_status")) {
+        String member_status = jsonObjectUser.getString("member_status");
+        if (user_type.trim().equalsIgnoreCase("carrier")) {
+          System.out.println("car===" + member_status);
+          sessionManager.setTransporterStatus(member_status);
+        }
+      }*/
+
+
+
+    }else if(data["ResponseCode"]=="400")
+    {
+    //  _showMyDialog(ctx,data["Message"],);
+    }
+
+
+
+    // "ResponseCode":400,
+
+    return "";
+    //return Album.fromJson(jsonDecode(response.body));
+  } else {
+    print("in future class excption ");
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
   }
+}
+
+
+//show already login dialog
+Future<void> _showMyDialog(BuildContext context, String message,String userID,String GUID) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('AlertDialog Title'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children:  <Widget>[
+              Text(message),
+              Text('Do you want to Login on this device and logout from another device?'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('ok'),
+            onPressed: () {
+            //  Navigator.of(context,rootNavigator: true).pop();
+               var futureLoginCheck = getLoginCheck(context,userID,GUID);
+
+
+
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'Login_Screen.dart';
 import 'package:http/http.dart' as http;
-
-
+import 'DashBoardScreen.dart';
+import 'Login_Screen.dart';
 import 'dart:async';
 
 
@@ -46,19 +46,30 @@ class SplashScreenState extends State<MyHomePage> {
 
  // }
 
+
   void asyncMethod() async {
     SharedPreferences prefs =  await SharedPreferences.getInstance();
     var GUID= prefs.getString('GUID').toString();
-    if(GUID!=null)
+    var userId= prefs.getString('Userid').toString();
+    print("guid from pref"+GUID+" "+userId);
+    if(GUID == null || GUID == "null" || GUID == "")
     {
-      Timer(Duration(seconds: 5),
-              ()=>Navigator.pushReplacement(context,
-              MaterialPageRoute(builder: (context) => LoginPage()
-              )
-          )
-      );
-    }else{
       var futureAlbum = fetchAlbum(context);
+
+    }else if(userId==null || userId == "null" || userId == "")
+      {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => LoginPage()),
+        );
+      }
+
+    else{
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => DashBoardScreen()),
+      );
     }
 
     // ....
@@ -92,30 +103,25 @@ class HomeScreen extends StatelessWidget {
 }
 
 
-
+//API Call Post method
 Future<String> fetchAlbum(BuildContext ctx) async {
-  print("in future class");
-  /*final response = await http
-      .post(Uri.parse('https://acmeitsolutions.net/projects/freightseek/api/registerDevice'));
-*/
   var jsonBody = { 'GUID': 'NEW'};
   Map<String, String> headers = {"Content-type": "application/x-www-form-urlencoded"};
   var baseUrl = 'http://acmeitservices.com/projects/freightseek/api/';
   final uri = Uri.parse("https://acmeitsolutions.net/projects/freightseek/api/registerDevice");
   final response = await http.post(uri, headers: headers, body: jsonBody);
   print("in future class after api"+response.body);
-
   if (response.statusCode == 200) {
     var data = json.decode(response.body);
-    var userID = data["data"];
-    print("in future class 200 "+userID["GUID"]);
+    var dataGUID = data["data"];
+    print("in future class 200 "+dataGUID["GUID"]);
 
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString('GUID', userID["GUID"]);
+    prefs.setString('GUID', dataGUID["GUID"]);
     print("pref getstring=="+prefs.getString("GUID").toString());
    final BuildContext context;
     Timer(Duration(seconds: 5),
-            ()=>Navigator.pushReplacement(ctx,
+            ()=>Navigator.push(ctx,
             MaterialPageRoute(builder: (context) => LoginPage()
             )
         )
@@ -126,7 +132,6 @@ Future<String> fetchAlbum(BuildContext ctx) async {
     //return Album.fromJson(jsonDecode(response.body));
   } else {
     print("in future class excption ");
-
     // If the server did not return a 200 OK response,
     // then throw an exception.
     throw Exception('Failed to load album');
